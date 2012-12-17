bd0e8f6a7294357d2c1984c641f7111565305d42
Irina Vershinina <irinav.vershinina@gmail.com>
2012-12-17 22:28:15 +0400

    exercise6_1

---
+++ exercises/src/exercise6_1/echo_server.erl
@@ -0,0 +1,38 @@
+%% @author ivershinina
+%% @doc @todo Add description to echo_server.
+
+
+-module(echo_server).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([start/0, stop/0, print/1, loop/0]).
+
+start() ->
+    register(echo, spawn_link(echo_server, loop, [])),
+    ok.	
+
+print(Term) ->
+    echo ! {print, Term},
+    ok.
+
+loop() ->
+    receive 
+        {print, Term} -> 
+            io:format("~p~n", [Term]),
+            loop();
+        stop -> 
+            unregister(echo),
+            ok;
+        _Other -> {error, unknown_message}
+    end.
+
+stop() ->
+    exit(exit_signal),
+    ok.
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+
